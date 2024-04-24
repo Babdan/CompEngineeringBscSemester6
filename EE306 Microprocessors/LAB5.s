981b555268be78616765285f27f131301300498b
Bogdán Itsám Dorantes-Nikolaev <bogdik.bgd@gmail.com>
2024-04-24 15:30:06 +0300

    Update LAB5.s

--- EE306 Microprocessors/LAB5.s
+++ EE306 Microprocessors/LAB5.s
@@ -1,5 +1,7 @@
 //LAB5 BY: BOGDAN ITSAM DORANTES-NIKOLAEV, ONUR KELES, OMER MERT YILDIZ
 //----------------+++++<PART ONE>+++++----------------
+
+//-------------------------VARIANT "A"
 .text
 .global _start
 
@@ -29,6 +31,36 @@ sub_loop_off:
     BNE sub_loop_off             @ Continue looping until the counter is 0
 
     B _start                     @ Loop back to the start
-	
 
+//-------------------------VARIANT "B"
+
+.text
+.global _start
+
+.equ LED_BASE, 0xFF200000  @ Define the base address for the LED
+
+_start:
+    LDR R0, =LED_BASE      @ Load the base address of the LED
+
+    @ Main loop starts here
+loop:
+    MOV R1, #1             @ Prepare to turn on the LED
+    STR R1, [R0]           @ Turn on the LED
+
+    @ Delay loop for ON state
+    LDR R7, =0250000       @ Set delay counter for approximately 0.25 seconds
+sub_loop_on:
+    SUBS R7, R7, #1        @ Decrement the counter
+    BNE sub_loop_on        @ Continue looping until the counter hits 0
+
+    MOV R1, #0             @ Prepare to turn off the LED
+    STR R1, [R0]           @ Turn off the LED
+
+    @ Delay loop for OFF state
+    LDR R7, =0250000       @ Set delay counter for approximately 0.25 seconds
+sub_loop_off:
+    SUBS R7, R7, #1        @ Decrement the counter
+    BNE sub_loop_off       @ Continue looping until the counter hits 0
+
+    B loop                 @ Repeat the main loop
 //----------------+++++<PART TWO>+++++----------------
